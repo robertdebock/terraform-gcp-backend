23612400ebf1aadc2a275fb525607d1d2a9d57c7
Robert de Bock <robert@meinit.nl>
2022-04-06 12:10:13 +0200

    Comments.

--- bucket/main.tf
+++ bucket/main.tf
@@ -1,3 +1,4 @@
+# Buckets must have a unique name, so generate some random string.
 resource "random_string" "default" {
   length  = 6
   number  = false
@@ -8,6 +9,8 @@ resource "random_string" "default" {
 resource "google_storage_bucket" "default" {
   name                        = "terraform-state-${random_string.default.result}"
   location                    = "EU"
+  # The `force_destroy` is `false` by default, not allowing `destroy`.
   force_destroy               = true
+  # Some policies may require `uniform_bucket_level_access`.
   uniform_bucket_level_access = true
 }
