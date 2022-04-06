6551d2f428eb68343b9ec2e94d2daa7e784307e4
Robert de Bock <robert@meinit.nl>
2022-04-06 12:08:03 +0200

    fmt

--- bucket/output.tf
+++ bucket/output.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
   description = "The name of the bucket."
-  value = google_storage_bucket.default.id
+  value       = google_storage_bucket.default.id
 }
--- bucket/providers.tf
+++ bucket/providers.tf
@@ -5,7 +5,7 @@ terraform {
       version = "4.13.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.1.2"
     }
   }
